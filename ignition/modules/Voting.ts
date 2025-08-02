4001342dbd6dde9dc85c41c73602330ebaa92bb0
zeative <zaadevofc@gmail.com>
2025-08-02 20:14:33 +0700

    smart contract

--- ignition/modules/Voting.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const VotingModule = buildModule("VotingModule", (m) => {
-  const voting = m.contract("Voting");
-  return { voting };
-});
-
-export default VotingModule;
